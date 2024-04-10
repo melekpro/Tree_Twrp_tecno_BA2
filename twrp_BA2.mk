c2c5ebd666a04f4e5e3a6a76182f07a95f60acf6
melekpro <melekpro6@gmail.com>
2024-04-10 16:57:37 -0700

    Update twrp_BA2.mk

--- twrp_BA2.mk
+++ twrp_BA2.mk
@@ -14,7 +14,7 @@ PRODUCT_COPY_FILES += \
     device/tecno/BA2/recovery.fstab:recovery/root/etc/recovery.fstab
 
 PRODUCT_DEVICE := BA2
-PRODUCT_NAME := omni_BA2
+PRODUCT_NAME := twrp_BA2
 PRODUCT_BRAND := tecno
 PRODUCT_MODEL := BA2
 PRODUCT_MANUFACTURER := tecno
